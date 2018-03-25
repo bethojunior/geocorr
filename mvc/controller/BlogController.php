da63040cb14ee056cd80b5c293e90ab55ca76ab4
betho <hgomescostajr@gmail.com>
2018-03-25 14:06:55 -0300

    name variavel

--- mvc/controller/BlogController.php
+++ mvc/controller/BlogController.php
@@ -9,24 +9,24 @@
 
     try{
         $conn = Connection::open(Connection::DB_MYSQL);
-        $query = ("INSERT INTO blog (titleBlog , postBlog , dataBlog) VALUES (:titleBlog , :postBlog , :dataBlog) ");
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(':titleBlog' , $titleBlog ,  PDO::PARAM_STR);
-        $stmt->bindParam(':postBlog'  , $postBlog ,  PDO::PARAM_STR);
-        $stmt->bindParam(':dataBlog'  , $dataBlog ,  PDO::PARAM_STR);
+        $sql = ("INSERT INTO blog (titleBlog , postBlog , dataBlog) VALUES (:titleBlog , :postBlog , :dataBlog) ");
+        $query = $conn->prepare($sql);
+        $query->bindParam(':titleBlog' , $titleBlog ,  PDO::PARAM_STR);
+        $query->bindParam(':postBlog'  , $postBlog ,   PDO::PARAM_STR);
+        $query->bindParam(':dataBlog'  , $dataBlog ,   PDO::PARAM_STR);
         
-        $stmt -> execute();
+        $query -> execute();
 
-        if($stmt){
-            echo json_encode(['result' => true]);
+        if($query){
+            echo   json_encode(['result' => true]);
             return json_encode(['result' => true]);
         }else {
-            echo json_encode(['result' => false]);
+            echo   json_encode(['result' => false]);
             return json_encode(['result' => false]);
         }
     
     }catch(Exception $e){
-        echo json_encode(['result' => false]);
+        echo   json_encode(['result' => false]);
         return json_encode(['result' => false]);
     }
 
