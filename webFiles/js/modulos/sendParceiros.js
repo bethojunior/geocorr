d2be8ac4fc984b1a30528f01f2b73f7428058b2c
betho <hgomescostajr@gmail.com>
2018-04-10 12:24:26 -0300

    ok

---
+++ webFiles/js/modulos/sendParceiros.js
@@ -0,0 +1,17 @@
+//document.addEventListener("DOMContentLoaded", function(){
+    function preview() {
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            // get loaded data and render thumbnail.
+            document.getElementById("image-holder").src = e.target.result;
+        };
+
+        // read the image file as a data URL.
+        reader.readAsDataURL(this.files[0]);
+    };
+
+
+
+
